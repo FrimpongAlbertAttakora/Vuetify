ee3e8c27767f8a54047b4de021e96cd2e1ce3dcc
unknown <kwaafjets2310@gmail.com>
2019-12-11 11:58:19 +0000

    First commit

---
+++ src/components/appToolbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-toolbar 
+    color = 'indigo' dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>frimpsTech</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+       <v-toolbar-items>
+        <v-btn text>About</v-btn>
+        <v-btn text>Contact</v-btn>
+        <v-btn text>Login</v-btn>
+      </v-toolbar-items>
+
+    </v-toolbar>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <appToolbar msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import appToolbar from '../components/appToolbar.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    appToolbar
   }
 }
 </script>
